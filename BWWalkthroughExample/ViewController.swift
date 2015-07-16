d8d103e91587f23b21ccb422ac9c8453071e1ffe
Vadim Drobinin <vadim@drobinin.com>
2015-07-16 20:34:07 +0400

    Prepare example

--- BWWalkthroughExample/ViewController.swift
+++ BWWalkthroughExample/ViewController.swift
@@ -8,59 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController, BWWalkthroughViewControllerDelegate {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        let userDefaults = NSUserDefaults.standardUserDefaults()
-        
-        if !userDefaults.boolForKey("walkthroughPresented") {
-            
-            showWalkthrough()
-            
-            userDefaults.setBool(true, forKey: "walkthroughPresented")
-            userDefaults.synchronize()
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
-    @IBAction func showWalkthrough(){
-        
-        // Get view controllers and build the walkthrough
-        let stb = UIStoryboard(name: "Walkthrough", bundle: nil)
-        let walkthrough = stb.instantiateViewControllerWithIdentifier("walk") as! BWWalkthroughViewController
-        let page_zero = stb.instantiateViewControllerWithIdentifier("walk0") as! UIViewController
-        let page_one = stb.instantiateViewControllerWithIdentifier("walk1") as! UIViewController
-        let page_two = stb.instantiateViewControllerWithIdentifier("walk2")as! UIViewController
-        let page_three = stb.instantiateViewControllerWithIdentifier("walk3") as! UIViewController
-        
-        // Attach the pages to the master
-        walkthrough.delegate = self
-        walkthrough.addViewController(page_one)
-        walkthrough.addViewController(page_two)
-        walkthrough.addViewController(page_three)
-        walkthrough.addViewController(page_zero)
-        
-        self.presentViewController(walkthrough, animated: true, completion: nil)
-    }
-    
+extension ViewController: BWWalkthroughViewControllerDelegate {
     
     // MARK: - Walkthrough delegate -
     
     func walkthroughPageDidChange(pageNumber: Int) {
         println("Current Page \(pageNumber)")
     }
-
+    
     func walkthroughCloseButtonPressed() {
         self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
+
+class ViewController: UIViewController {
+    
+    let stb = UIStoryboard(name: "Walkthrough", bundle: nil)
+    let pattern = "walk"
+    let numberOfViewControllers = 10
+    
+    var vcs = [UIViewController]()
+    var walkthrough: BWWalkthroughViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        walkthrough = stb.instantiateViewControllerWithIdentifier(pattern) as! BWWalkthroughViewController
+        walkthrough.delegate = self
+    }
+    
+    @IBAction func showWalkthrough(){
+        for idx in 0..<numberOfViewControllers {
+            vcs.append(stb.instantiateViewControllerWithIdentifier(pattern + "\(idx)") as! UIViewController)
+        }
+        
+        for idx in 1..<numberOfViewControllers {
+            walkthrough.addViewController(vcs[idx])
+        }
+        walkthrough.addViewController(vcs[0])
+        
+        self.presentViewController(walkthrough, animated: true, completion: nil)
     }
     
 }
